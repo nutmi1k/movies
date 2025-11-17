80a09755aa99c1709d0984bdd2877e731f0d1c6b
Otten, Derek D <derek.d.otten@chase.com>
2025-11-17 14:04:00 -0600

    dummy data to see how its formatted

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.getElementById('myButton').onclick = function() {
     alert('Click.');
 };
+
+const films = [
+    { name: "The Fast and the Furious", year: 30, rating: 5 },
+    { name: "Scream", year: 1996, rating: 4 },
+    { name: "Tommy Boy", year: 1995, rating: 5 }
+];
+
+const tbody = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+
+films.forEach(film => {
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${film.name}</td><td>${film.year}</td><td>${film.rating}</td>`;
+    tbody.appendChild(row);
+});
